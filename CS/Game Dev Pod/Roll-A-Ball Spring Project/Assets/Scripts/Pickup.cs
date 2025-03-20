c4621824c8d71adc33a3061c5051d8a0813f994e
MAxfox-0 <maxwellesayas@gmail.com>
2025-03-19 18:14:51 -0700

    Moving things around

--- CS/Game Dev Pod/Roll-A-Ball Spring Project/Assets/Scripts/Pickup.cs
+++ CS/Game Dev Pod/Roll-A-Ball Spring Project/Assets/Scripts/Pickup.cs
@@ -21,7 +21,7 @@ public class Pickup : MonoBehaviour
     // 1. Declare a private variable of type bool named 'isCollected' and assign it the value 'false';
     // This variable is a flag to trak whether or not this pickup has been collected by the player or not
     // YOUR CODE HERE
-
+    private bool isCollected = false;
     // Update is a built-in Unity function/method called every frame of your game (there is on average 60 frames per second)
     void Update()
     {
@@ -30,6 +30,7 @@ public class Pickup : MonoBehaviour
         // This statement makes this GameObject rotate 15 degrees on the x-axis, 30 on the y-axis, & 45 on the z-axis every second; 
         // (Time.deltaTime is a special number that makes an operation in Update() happen per second rather than per frame, creating a more consistent performance across many different frame rates)
         // YOUR CODE HERE
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
     }
 
     // OnTriggerEnter(Collider other) is a built-in Unity function/method that is called when another collider enters a trigger collider attached to the GameObject where this script is applied.
@@ -40,13 +41,23 @@ public class Pickup : MonoBehaviour
         // This if statement checks to see if the player has already collected a pickup to avoid the player from picking up the same item more than once
         // The 'return' keyword exits this OnTriggerEnter() method/function early since we do not want to calculate picking up a pickup item more than once
         // YOUR CODE HERE
-
+        if (isCollected == true)
+        {
+            return;
+        }
+        
         // 4. Write an if statement with the following condition: access 'other.gameObject.CompareTag()' 
         // Pass "Player" as a parameter into the the CompareTag() method/function
         // This if statement verifies what GameObject just collided with this pickup by checking if it has the tag "Player"; 
         // (HINT: Don't forget to create brackets{} for the body of your if statement; #5-8 should be inside and indented within these brackets{})
         // YOUR CODE HERE
-
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isCollected = true;
+            AudioManager.Instance.PlaySound("Collect Coin");
+            GameManager.Instance.UpdateScore(1);
+            Destroy(this.gameObject);
+        }
             // 5. Reassign the 'isCollected' variable the value 'true';
             // Only after we have confirmed the player character has touched this pickup can we mark it as collected 
             // YOUR CODE HERE
